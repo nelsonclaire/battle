c8a06ec7c77d341cf659645ccd142efde031ec13
Chris Middleton <ctjmiddleton@gmail.com>
2022-02-09 16:57:04 +0000

    post-redirect-get on names

--- app.rb
+++ app.rb
@@ -6,16 +6,27 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # our routes would go here
+  enable :sessions
+
   get "/" do
     erb(:index)
   end
 
   post "/names" do
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+    # @player1 = params[:Player1]
+    # @player2 = params[:Player2]
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect to('/play')
+    # erb(:play)
+  end
+
+  get '/play' do
+    @player1 = session[:Player1]
+    @player2 = session[:Player2]
     erb(:play)
   end
+
 
   # # Start the server if this file is executed directly (do not change the line below)
   run! if app_file == $0
