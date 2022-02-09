a05600d1f94bceed5c88bbb99780f476ebfbf13e
Claire Nelson <claire.nelson@johnlewis.co.uk>
2022-02-09 14:26:55 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes would go here
+  get "/" do
+    "Hello Battle!"
+  end
+
+  # # Start the server if this file is executed directly (do not change the line below)
+  run! if app_file == $0
+end
